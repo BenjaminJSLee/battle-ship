b69bb36fee85bf7a4e764de004d5fa6edcfdb883
Benji Lee <Benji@Benjis-MacBook-Pro.local>
2020-12-05 13:28:05 -0800

    Add gitignore, sounds, views

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+const PORT = 3000;
+
+app.set('view engine','ejs');
+
+app.get('/', (req, res) => {
+  res.render('ship_index',req.tempVars);
+});
+
+app.listen(PORT,()=>{
+  console.log(`server is running on port ${PORT}`);
+});
