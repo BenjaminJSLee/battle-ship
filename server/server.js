916f1ed49d2710dc5fc6a50958e18f82aac039d9
Benjamin Lee <benjaminjslee@gmail.com>
2020-12-09 21:33:40 -0800

    Add more framework, sounds, and dependencies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const sass = require("node-sass-middleware");
 const app = express();
 
 const PORT = 3000;
 
 app.set('view engine','ejs');
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.render('ship_index',req.tempVars);
+});
+
+app.get('/:id', (req, res) => {
+  res.render('ship_game');
 });
 
 app.listen(PORT,()=>{
